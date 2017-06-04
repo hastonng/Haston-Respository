4f71765bb6577dd4fef7454abdefbaf9ecc22bdc
hastonng <hastonng@gmail.com>
2017-06-04 14:57:11 +0800

    ExpensesClaimRequestSystem
    Expenses Claim Request System (ECRS), the system that helps end-user to make a request for claiming expenses in the company, allows uppermanagement to approve the request as well as checking the list of requests

---
+++ AssetRequestSystem/ViewController/src/backing/backing_ECRSMain.java
@@ -0,0 +1,12 @@
+package backing;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class backing_ECRSMain {
+    public backing_ECRSMain() {
+        
+    }
+}
