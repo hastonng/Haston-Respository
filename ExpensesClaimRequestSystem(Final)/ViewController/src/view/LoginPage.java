e51f4b897fc632f55b1c6732eff9e6b0e92a5b23
hastonng <hastonng@gmail.com>
2017-06-22 22:25:11 +0800

    Added New Page and Java Classes
    JSPX Page:
    - ECRSRequesterPage
    - ECRSApproverPage
    
    Java Class:
    - DBRetrieve
    - LoginPage

--- ExpensesClaimRequestSystem(Final)/ViewController/src/view/LoginPage.java
+++ ExpensesClaimRequestSystem(Final)/ViewController/src/view/LoginPage.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import java.io.PrintWriter;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.faces.component.html.HtmlOutputText;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -20,8 +24,9 @@ public class LoginPage extends HttpServlet
     static private RichPanelGroupLayout pgl1;
     static private RichForm f1;
     static private RichDocument d1;
-    static private HtmlOutputText ot1;
-    
+    static private HtmlOutputText resultText;
+    static private String outputStr;
+
 
     public LoginPage() {
     }
@@ -66,25 +71,66 @@ public class LoginPage extends HttpServlet
     public RichDocument getD1() {
         return d1;
     }
-
-
-    public void setOt1(HtmlOutputText ot1) {
-        this.ot1 = ot1;
-    }
-
-    public HtmlOutputText getOt1() {
-        return ot1;
-    }
     
+    public void setResultText(HtmlOutputText resultText) {
+        LoginPage.resultText = resultText;
+    }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException
-    {
+    public static HtmlOutputText getResultText() {
+        return resultText;
+    }
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException,
+                                                                                       SQLException {
             response.setContentType("text/html");
             PrintWriter out = response.getWriter(); 
             String uN = request.getParameter("username"); 
             String pW = request.getParameter("password"); 
+            ResultSet userResult,passResult; // Declaring DBRetrieve
+            DBRetrieve userQuery,passQuery; // Query for DBRetrieve
+            int role;
             
+            //Query Database for pass and user
             
+            userResult = userQuery.Query("SELECT * from ECRS_USER,WHERE USER_NAME='"+uN+"';");
+            passResult = passQuery.Query("SELECT * from ECRS_USER,WHERE PASSWORD='"+pW+"';");
+                
+            String UNResult = userResult.toString();
+            String PWResult = passResult.toString();
             
+            if(uN == UNResult ) {
+                
+                if(pW == PWResult){
+                    
+                    role = checkUserRole(uN);
+                    
+                    switch(role){
+                    
+                    case 1: break;
+                    
+                    case 2: out.print("<a href=C://Users//Haston Ng//Documents//GitHub//Haston-Respository//ExpensesClaimRequestSystem(Final)//ViewController//public_html//ecrs-requester-main-page.jspx");
+                            break;
+                    
+                    case 3: out.print("<a href=C://Users//Haston Ng//Documents//GitHub//Haston-Respository//ExpensesClaimRequestSystem(Final)//ViewController//public_html//ecrs-approver-main-page.jspx");
+                            break;
+                        
+                    }
+                }
+                
+            }
+            else
+                
+                           
+    }
+    
+    public int checkUserRole(String userName) throws SQLException {
+        DBRetrieve getRole;
+        ResultSet result;
+        
+        result=getRole.Query("SELECT USER_ROLE FROM ECRS_USER, WHERE USER_NAME='"+userName+"';");
+        int roleResult = result.getInt(0);
+        
+        return roleResult;
     }
 }
